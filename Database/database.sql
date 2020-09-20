330b8ef5d81984b9084a0998531d853c868acfb9
Victor Aquino <victor.aquino05@outlook.com>
2020-09-20 09:44:25 -0300

    adicionando tabelas

--- Database/database.sql
+++ Database/database.sql
@@ -8,3 +8,25 @@ CREATE TABLE author (
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE post (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author_id INT NOT NULL,
+    text TEXT NOT NULL,
+    time DATETIME NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES author(id)
+);
+
+CREATE TABLE tag (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE post_tags (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    post_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES post(id),
+    FOREIGN KEY (tag_id) REFERENCES tag(id)
+);
