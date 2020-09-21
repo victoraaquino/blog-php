e2a917348c2695cbd513688a25f22f8cab36dae1
Victor Aquino <victor.aquino05@outlook.com>
2020-09-20 23:10:10 -0300

    ajuste construtor tagDAO

--- Dao/TagDAO.php
+++ Dao/TagDAO.php
@@ -1,13 +1,37 @@
 <?php
 
 require_once "./../Model/Tag.php";
+require_once "./../Database/Connection.php";
 
-class TagDAO {
+class TagDAO
+{
 
     private $tag;
 
-    public function __construct(Tag $tag)
+    public function __construct()
+    {
+        $this->conn = Connection::getConnection();
+    }
+
+    public function setTag(Tag $tag)
     {
         $this->tag = $tag;
+    }
+
+    public function getTag()
+    {
+        return $this->tag;
+    }
+
+    //Retorna todos as tags cadastrados
+    public function getAll()
+    {
+        $stmt = $this->conn->prepare("SELECT * FROM tag");
+
+        $stmt->execute();
+
+        $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $res;
     }
 }
