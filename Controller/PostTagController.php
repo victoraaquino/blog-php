b146100bb55a5f324d8721703a3b01b5451af752
Victor Aquino <victor.aquino05@outlook.com>
2020-09-21 00:32:08 -0300

    deletar e carregar tags por post

--- Controller/PostTagController.php
+++ Controller/PostTagController.php
@@ -1,20 +1,45 @@
 <?php
 
-require_once "./../Model/PostTag.php";
-require_once "./../Model/Tag.php";
-require_once "./../Model/Post.php";
-require_once "./../Dao/PostTagDAO.php";
+require_once "../Model/PostTag.php";
+require_once "../Model/Tag.php";
+require_once "../Model/Post.php";
+require_once "../Dao/PostTagDAO.php";
+require_once "TagController.php";
 
-class PostTagController {
+class PostTagController
+{
 
-    public static function insert($data){
-
+    public static function getAllByPost($postId)
+    {
         $post = new Post();
-        $post->setId($data['post_id']);
+        $post->setId($postId);
 
+        $postTag = new PostTag();
+        $postTag->setPost($post);
+
+        $dao = new PostTagDAO();
+        $dao->setPostTag($postTag);
+
+        $res = $dao->getAllByPost();
+
+        $tags = [];
+
+        foreach ($res as $i => $resPostTag) {
+            $tags[] = TagController::getOne($resPostTag['tag_id']);
+        }
+
+        return $tags;
+    }
+
+    public static function insert($data)
+    {
+        var_dump($data['tags']);
         foreach ($data['tags'] as $tagId) {
             $tag = new Tag();
             $tag->setId($tagId);
+
+            $post = new Post();
+            $post->setId($data['post_id']);
 
             $postTag = new PostTag();
             $postTag->setPost($post);
@@ -25,7 +50,18 @@ class PostTagController {
 
             $dao->insert();
         }
-
     }
 
+    public static function removeByPost($postId)
+    {
+        $post = new Post();
+        $post->setId($postId);
+
+        $postTag = new PostTag();
+        $postTag->setPost($post);
+
+        $dao = new PostTagDAO();
+        $dao->setPostTag($postTag);
+        $dao->removeByPost();
+    }
 }
--- Dao/PostTagDAO.php
+++ Dao/PostTagDAO.php
@@ -15,12 +15,25 @@ class PostTagDAO
 
     public function setPostTag(PostTag $postTag)
     {
-        $this->$postTag = $postTag;
+        $this->postTag = $postTag;
     }
 
     public function getPostTag()
     {
         return $this->postTag;
+    }
+
+    public function getAllByPost()
+    {
+        $stmt = $this->conn->prepare("SELECT * FROM post_tag WHERE post_id=:post_id");
+
+        $stmt->bindValue(":post_id", $this->postTag->getPost()->getId());
+
+        $stmt->execute();
+
+        $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $res;
     }
 
     public function insert()
@@ -29,6 +42,17 @@ class PostTagDAO
 
         $stmt->bindValue(":post_id", $this->postTag->getPost()->getId());
         $stmt->bindValue(":tag_id", $this->postTag->getTag()->getId());
+
+        $res = $stmt->execute();
+
+        return $res;
+    }
+
+    public function removeByPost()
+    {
+        $stmt = $this->conn->prepare("DELETE FROM post_tag WHERE post_id=:post_id");
+
+        $stmt->bindValue(":post_id", $this->postTag->getPost()->getId());
 
         $res = $stmt->execute();
 
